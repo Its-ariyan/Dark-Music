c8f4080873ef9cfa0d90ef4939e0ec51821e39ec
ɪᴛs-ᴀʀɪʏᴀɴ <113695332+Prince-ariyan@users.noreply.github.com>
2022-12-16 01:51:42 +0600

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -84,7 +84,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@Client.on_message(filters.command(["song", "music", " vsong"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["song", "music", " vsong", "video"]) & ~filters.private & ~filters.channel)
 def song(client, message):
 
     message.delete()
@@ -115,17 +115,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "» ɴᴏᴛ ғᴏᴜɴᴅ, ᴛʀʏ sᴇᴀʀᴄʜɪɴɢ ᴡɪᴛʜ ᴛʜᴇ sᴏɴɢ ɴᴀᴍᴇ."
+            " ɴᴏᴛ ғᴏᴜɴᴅ, ᴛʀʏ sᴇᴀʀᴄʜɪɴɢ ᴡɪᴛʜ ᴛʜᴇ sᴏɴɢ ɴᴀᴍᴇ"
         )
         print(str(e))
         return
-    m.edit(f"» ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ sᴏɴɢ ꜰʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ sᴇʀᴠᴇʀ.")
+    m.edit(f"Downloading song form {bn} server.")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**• ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ​ » [ᴅᴀʀᴋxᴍᴜsɪᴄ](t.me/{BOT_USERNAME}) 🍄\n• ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ​ » {chutiya}\n• sᴇᴀʀᴄʜᴇᴅ ғᴏʀ » {query}**"
+        rep = f"** 🆙 ​ᴜᴘʟᴏᴀᴅ ʙʏ [{bn}](t.me/{bu}) 👤\n• ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ​  {chutiya}\n🔎 sᴇᴀʀᴄʜ ғᴏʀ  {query}**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -133,7 +133,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("**» ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴇʀʀᴏʀ, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ​ » [ᴛᴇᴄʜ ǫᴜᴀʀᴅ](t.me/TechQuard})**")
+        m.edit("** Downloading error, report this at  [{bn} support](t.me/{bu}) **")
         print(e)
 
     try:
